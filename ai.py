daa31fc01c89f185b80b577c37160a23c6e13e24
Fellipe Duarte <fefellipe03@gmail.com>
2023-12-06 21:05:40 -0300

    melhorias :D

--- ai.py
+++ ai.py
@@ -1,159 +1,151 @@
 import math
-from simpleHeap import SimpleHeap
-
 import pygame
+from boost import Boost
+from grass import Grass
+from road import Road
+from checkpoint import Checkpoint
+from heapq import heappush, heappop
 
 MAX_ANGLE_VELOCITY = 0.05
 BLOCK_SIZE = 50
-
+COST_MAPPING = {
+    Road: 3,
+    Checkpoint: 3,
+    Boost: 1,
+    Grass: 300
+}
 
 class AI():
-    """
-    A simple AI class for controlling a kart in a racing game.
-
-    Attributes:
-        kart: The kart object controlled by the AI.
-        checkpoints: The checkpoints on the track.
-    """
-
     def __init__(self):
-        """
-        Initialize the AI with default values.
-        """
         self.kart = None
+        self.graph = None
         self.checkpoints = None
 
     def move(self, string):
-        """
-        Move the kart based on the current track information.
 
-        Args:
-            string (str): The string describing the track.
+        if self.kart.next_checkpoint_id == 0:
+            checkpoint_char = 'C'
+        elif self.kart.next_checkpoint_id == 1:
+            checkpoint_char = 'D'
+        elif self.kart.next_checkpoint_id == 2:
+            checkpoint_char = 'E'
+        elif self.kart.next_checkpoint_id == 3:
+            checkpoint_char = 'F'
+        rows = string.split('\n')
+        rows = [row.strip() for row in rows if row.strip()]
+        for row_index in range(len(rows)):
+            row = rows[row_index]
+            for col_index in range(len(row)):
+                track_char = row[col_index]
+                if track_char == checkpoint_char:
+                    break
+            else:
+                continue
+            break
 
-        Returns:
-            dict: A dictionary of keys (UP, DOWN, LEFT, RIGHT) and corresponding boolean values.
-        """
+        next_checkpoint_position = [col_index * BLOCK_SIZE, row_index * BLOCK_SIZE + .5 * BLOCK_SIZE]
 
-        closest_checkpoint = self.find_closest_checkpoint(string)
-        next_move = self.get_minimum_valid_neighbor(string, self.kart.position[0], self.kart.position[1],
-                                                    closest_checkpoint[0], closest_checkpoint[1])
+        path = self.a_star(string, self.kart.position, next_checkpoint_position, self.h, self.neighbors)
 
-        if next_move:
-            next_point = next_move
+        if path:
+            next_point = path[1]
             relative_x = next_point[0] - self.kart.position[0]
             relative_y = next_point[1] - self.kart.position[1]
             next_point_angle = math.atan2(relative_y, relative_x)
             relative_angle = (next_point_angle - self.kart.angle + math.pi) % (2 * math.pi) - math.pi
 
-            # Adjust movement based on the relative angle
+            right = [False, False, False, True]
+            left = [False, False, True, False]
+            forward = [True, False, False, False]
+
             if relative_angle > MAX_ANGLE_VELOCITY:
-                command = [False, False, False, True]  # Turn right
+                command = right
             elif relative_angle < -MAX_ANGLE_VELOCITY:
-                command = [False, False, True, False]  # Turn left
+                command = left
             else:
-                command = [True, False, False, False]  # Move forward
+                command = forward
 
             key_list = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
             keys = {key: command[i] for i, key in enumerate(key_list)}
             return keys
         else:
-            return {pygame.K_UP: False, pygame.K_DOWN: False, pygame.K_LEFT: False, pygame.K_RIGHT: False}
+            no_movement = {pygame.K_UP: False, pygame.K_DOWN: False, pygame.K_LEFT: False, pygame.K_RIGHT: False}
+            return no_movement
 
-    def get_minimum_valid_neighbor(self, string, x, y, c_x, c_y):
+
+    def a_star(self, string, start, goal, h, neighbors):
         """
-        Get the minimum valid neighbor position based on the current kart position and checkpoint position.
+        A* Search Algorithm
 
-        Args:
-            string (str): The string describing the track.
-            x (float): The x-coordinate of the kart position.
-            y (float): The y-coordinate of the kart position.
-            c_x (float): The x-coordinate of the checkpoint position.
-            c_y (float): The y-coordinate of the checkpoint position.
+        A* is an informed search algorithm that uses a combination of the cost to
+        reach a node (goal), and an estimated cost (h) from the current node to
+        the goal, to determine the priority of nodes in the search.
 
-        Returns:
-            tuple: The coordinates of the minimum valid neighbor position.
+        Wikipedia page: https://en.wikipedia.org/wiki/A*_search_algorithm
         """
-        # when you increase this value, the kart performs better but slows the code, I recommend 30
-        degree_level = 50
-        x, y = int(x), int(y)
-        rows = len(string)
-        cols = len(string[0])
+        def reconstruct_path(cameFrom, current):
+            total_path = [current]
+            while current in cameFrom:
+                current = cameFrom[current]
+                total_path.insert(0, current)
+            return total_path
 
-        possible_neighbors = {
-            (int(x + i), int(y + j))
-            for i in range(-degree_level, degree_level + 1)
-            for j in range(-degree_level, degree_level + 1)
-            if i != 0 or j != 0
-        }
-        valid_neighbors = (
-            (nx, ny)
-            for nx, ny in possible_neighbors
-            if self.kart.get_track_element(string, nx, ny)[0].__name__ in ("Road", "Boost", "Checkpoint", "Grass")
-        )
+        openSet = [(0, start)]
+        cameFrom = {}
+        gScore = {tuple(start): 0}
+        fScore = {tuple(start): h(string, start, goal)}
 
-        priority_queue = SimpleHeap()
+        while openSet:
+            _, current = heappop(openSet)
+            if tuple(current) == tuple(goal):
+                return reconstruct_path(cameFrom, tuple(current))
 
-        for nx, ny in valid_neighbors:
-            cost = self.calculate_cost(string, nx, ny, c_x, c_y)
-            SimpleHeap.push(priority_queue, (cost, (nx, ny)))
-        min_valid_neighbor = SimpleHeap.pop(priority_queue)[1]
+            for neighbor in neighbors(string, current):
+                # linhas com problemas
+                neighbor_track_element = self.kart.get_track_element(string, neighbor[0], neighbor[1])[0]
+                cost = COST_MAPPING.get(neighbor_track_element)
+                tentative_gScore = gScore[tuple(current)] + cost
+                # acabou aqui
+                if tentative_gScore < gScore.get(tuple(neighbor), float('inf')):
+                    cameFrom[tuple(neighbor)] = tuple(current)
+                    gScore[tuple(neighbor)] = tentative_gScore
+                    fScore[tuple(neighbor)] = tentative_gScore + h(string, neighbor, goal)
+                    heappush(openSet, (fScore[tuple(neighbor)], tuple(neighbor)))
 
-        return min_valid_neighbor
+        return None
 
-    def calculate_cost(self, string, nx, ny, c_x, c_y):
-        track_element = self.kart.get_track_element(string, nx, ny)[0].__name__
-        distance_to_checkpoint = abs(nx - c_x) + abs(ny - c_y)
+    def h(self, string, current, goal):
 
-        if track_element in ("Road", "Boost", "Checkpoint"):
-            # Add 1 for road, boost, and checkpoint
-            return 1 + distance_to_checkpoint
-        elif track_element == "Grass":
-            # Add 100 for grass and distance to checkpoint
-            return 100 + distance_to_checkpoint
+        manhattan_distance = abs(int(current[0]) - int(goal[0])) + abs(int(current[1]) - int(goal[1]))
+        kart_speed = self.kart.get_speed()
 
-    def find_closest_checkpoint(self, string):
-        """
-        Find the closest checkpoint with the correct ID to the kart's current position.
+        speed_x, speed_y = self.kart.get_speed()
+        kart_speed = math.sqrt(speed_x**2 + speed_y**2)
+        track_element, _ = self.kart.get_track_element(string, current[0], current[1])
+        terrain_penalty = 1
 
-        Returns:
-            tuple: The position of the closest checkpoint.
-        """
-        kart_x, kart_y = self.kart.position[0], self.kart.position[1]
-        kart_next_checkpoint_id = self.kart.next_checkpoint_id
+        if track_element == Grass:
+            terrain_penalty = 2
 
-        checkpoints = []
+        combined_heuristic = manhattan_distance + terrain_penalty * kart_speed
 
-        char = ''
-        if kart_next_checkpoint_id == 0:
-            char = 'C'
-        elif kart_next_checkpoint_id == 1:
-            char = 'D'
-        elif kart_next_checkpoint_id == 2:
-            char = 'E'
-        elif kart_next_checkpoint_id == 3:
-            char = 'F'
+        return combined_heuristic
 
-        rows = string.split('\n')
-        rows = [row.strip() for row in rows if row.strip()]
+    def neighbors(self, string, current):
+        x, y = current
+        possible_neighbors = [
+            (x + 1, y),
+            (x - 1, y),
+            (x, y + 1),
+            (x, y - 1),
+            (x - 1, y - 1),
+            (x - 1, y + 1),
+            (x + 1, y - 1),
+            (x + 1, y + 1)
+        ]
 
-        for row_index in range(len(rows)):
-            row = rows[row_index]
-            for col_index in range(len(row)):
-                track_char = row[col_index]
-                if track_char == char:
-                    checkpoints.append((col_index * BLOCK_SIZE + 0.5 * BLOCK_SIZE,
-                                        row_index * BLOCK_SIZE + 0.5 * BLOCK_SIZE))
-
-        closest_checkpoint = None
-        min_distance = float('inf')
-
-        for checkpoint in checkpoints:
-            c_x, c_y = checkpoint
-            distance_to_checkpoint = abs(kart_x - c_x) + abs(kart_y - c_y)
-
-            if distance_to_checkpoint < min_distance:
-                min_distance = distance_to_checkpoint
-                closest_checkpoint = checkpoint
-
-        return closest_checkpoint
-
+        valid_neighbors = [
+            (nx, ny) for nx, ny in possible_neighbors
+            if self.kart.get_track_element(string, nx, ny)[0] in (Road, Boost, Checkpoint, Grass)
+        ]
+        return valid_neighbors
--- kart.py
+++ kart.py
@@ -1,11 +1,16 @@
 import math
-
 import pygame
+
+from boost import Boost
+from checkpoint import Checkpoint
+from grass import Grass
+from lava import Lava
+from road import Road
+from track import Track
 
 from kartPhysics import KartPhysics
 from raceEventHandler import RaceEventHandler
 
-from track import Track
 
 MAX_ANGLE_VELOCITY = 0.05
 MAX_ACCELERATION = 0.25
@@ -15,24 +20,6 @@ BOOST_VELOCITY = 25
 
 
 class Kart(KartPhysics, RaceEventHandler):
-    """
-    Class implementing the display and physics of the kart in the game.
-
-    Attributes:
-        has_finished (bool): Flag indicating whether the kart has finished the race.
-        controller: The controller object associated with the kart.
-        position (tuple): Current position of the kart (x, y).
-        last_position (tuple): Previous position of the kart (x, y).
-        start_position (tuple): Initial position of the kart.
-        start_orientation (float): Initial orientation of the kart.
-        last_speed (tuple): Last recorded speed of the kart (delta_x, delta_y).
-        angle (float): Current orientation angle of the kart.
-        last_angle (float): Previous orientation angle of the kart.
-        current_acceleration (float): Current acceleration of the kart.
-        next_checkpoint_id (int): ID of the next checkpoint the kart should pass.
-        orientation_from_checkpoint (float): Orientation angle recorded at the last checkpoint.
-        position_from_checkpoint (tuple): Position recorded at the last checkpoint.
-    """
 
     def __init__(self, controller):
         super().__init__()
@@ -52,13 +39,7 @@ class Kart(KartPhysics, RaceEventHandler):
         self.last_angle = 0
 
     def reset(self, initial_position, initial_orientation):
-        """
-        Reset the kart to the specified initial position and orientation.
 
-        Args:
-            initial_position (tuple): The initial position of the kart (x, y).
-            initial_orientation (float): The initial orientation angle of the kart.
-        """
         self.last_position = initial_position
         self.position = initial_position
 
@@ -77,24 +58,23 @@ class Kart(KartPhysics, RaceEventHandler):
             self.current_acceleration -= MAX_ACCELERATION
 
     def turn_left(self):
-        # 0(t) = 0(t-1) - v0
         self.last_angle = self.angle
         self.angle -= MAX_ANGLE_VELOCITY
 
     def turn_right(self):
-        # 0(t) = 0(t-1) + v0
         self.last_angle = self.angle
         self.angle += MAX_ANGLE_VELOCITY
+
+    def get_speed(self):
+        return self.last_speed
 
     def get_track_element(self, string, x, y):
         rows = string.split('\n')
         rows = [row.strip() for row in rows if row.strip()]
 
-        # Convert position indices to integers
         position_y = int(y)
         position_x = int(x)
 
-        # Calculate row and column indices
         row_index = position_y // BLOCK_SIZE
         col_index = position_x // BLOCK_SIZE
 
@@ -102,13 +82,11 @@ class Kart(KartPhysics, RaceEventHandler):
             row = rows[row_index]
 
             if 0 <= col_index < len(row):
-                # Use the calculated indices to access the 2D array
                 track_string = row[col_index]
                 track_element = Track.char_to_track_element.get(track_string, None)
                 if track_element is not None:
                     return track_element['class'], track_element.get('params', None)
 
-        # Outside the track behaves like lava
         return Track.char_to_track_element['L']['class'], Track.char_to_track_element['L'].get('params', None)
 
     def update_position(self, string, screen):
@@ -117,35 +95,29 @@ class Kart(KartPhysics, RaceEventHandler):
         delta_y = self.position[1] - self.last_position[1]
         self.last_speed = (delta_x, delta_y)
 
-        # Save the current position as the previous position
         self.last_position = self.position
-        if track_class.__name__ == "Road":
+        if track_class == Road:
             self.position = (self.calculate_x(0.02), self.calculate_y(0.02))
-        elif track_class.__name__ == "Grass":
+        elif track_class == Grass:
             self.position = (self.calculate_x(0.2), self.calculate_y(0.2))
-        elif track_class.__name__ == "Boost":
-            # Set velocity to 25 in formulas 3 and 4 for Boost
+        elif track_class == Boost:
             self.position = (self.calculate_x(0.02, velocity=BOOST_VELOCITY), self.calculate_y(0.02, velocity=BOOST_VELOCITY))
-        elif track_class.__name__ == "Checkpoint":
+        elif track_class == Checkpoint:
             self.position = (self.calculate_x(0.02), self.calculate_y(0.02))
             self.handle_checkpoint(track_params, string)
-        elif track_class.__name__ == "Lava":
+        elif track_class == Lava:
             self.handle_lava()
 
-        # Set to zero in case was not used
         self.current_acceleration = 0
 
     def draw(self, screen):
         kart_position = [self.position[0], self.position[1]]
 
-        # Draw the kart (circle)
         pygame.draw.circle(screen, (255, 255, 255), kart_position, KART_RADIUS)
 
-        # Calculate vertices of the triangle
-        triangle_size = 20  # Adjust the size of the triangle as needed
-        angle_offset = math.pi / 2  # Offset to point the triangle in the right direction
+        triangle_size = 20
+        angle_offset = math.pi / 2
 
-        # Calculate the vertices of the triangle based on the angle
         vertices = [
             (
                 kart_position[0] + int(triangle_size *
