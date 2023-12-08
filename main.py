ac2af23f99cbe7f75da290f6ebe864510dba6489
Fellipe Duarte <fefellipe03@gmail.com>
2023-12-07 21:19:37 -0300

    added if name == main

--- main.py
+++ main.py
@@ -5,34 +5,39 @@ from kart import Kart
 
 BLOCK_SIZE = 50
 
-track =  """GGGGGGGGGGGGGGGGGGGGGGGGGG
-            GRRRRRRCRRRRRRRRRBRRRRRRRG
-            GRRRRRRCRRRRRRRRRBRRRRRRRG
-            GRRRRRRCRRRRRRRRRRRRRRRRRG
-            GRRRRRRCRRRRRRRRRRRRRRRRRG
-            GGGGGGGGGGGGGGGGGGGGGRRRRG
-            GGGGGGGGGGGGGGGGGGGGGRRRRG
-            GRRRRGGGGGGGGGGGGGGGGRRRRG
-            GFFRRGGGGGGGGGGGGGGGGRRRRG
-            GLRRRGGGGGGGGGGGGGGGGRRRRG
-            GRRRRGGGGGGGGGGGGGGGGDDDDG
-            GRRRRRERRRRRRRBRRRRRRRRLLG
-            GRRRRRERRRRRRRBRRRRRRRRRRG
-            GLRRRRERRRRRGGBRRRRRRRRRRG
-            GLLRRRERRRRRGGBRRRRRRRRRRG
-            GGGGGGGGGGGGGGGGGGGGGGGGGG"""
+def main():
 
-kart_initial_position = [75, 75]
-kart_initial_angle = 0
+    track =  """GGGGGGGGGGGGGGGGGGGGGGGGGG
+                GRRRRRRCRRRRRRRRRBRRRRRRRG
+                GRRRRRRCRRRRRRRRRBRRRRRRRG
+                GRRRRRRCRRRRRRRRRRRRRRRRRG
+                GRRRRRRCRRRRRRRRRRRRRRRRRG
+                GGGGGGGGGGGGGGGGGGGGGRRRRG
+                GGGGGGGGGGGGGGGGGGGGGRRRRG
+                GRRRRGGGGGGGGGGGGGGGGRRRRG
+                GFFRRGGGGGGGGGGGGGGGGRRRRG
+                GLRRRGGGGGGGGGGGGGGGGRRRRG
+                GRRRRGGGGGGGGGGGGGGGGDDDDG
+                GRRRRRERRRRRRRBRRRRRRRRLLG
+                GRRRRRERRRRRRRBRRRRRRRRRRG
+                GLRRRRERRRRRGGBRRRRRRRRRRG
+                GLLRRRERRRRRGGBRRRRRRRRRRG
+                GGGGGGGGGGGGGGGGGGGGGGGGGG"""
 
-controller = AI()  # ou AI()
-"""
-==================== ATTENTION =====================
-In the original requirement, the lines of code below
-were indicated not to be changed.
-====================================================
-"""
-kart = Kart(controller)
-track = Track(track, kart_initial_position, kart_initial_angle)
-track.add_kart(kart)
-track.play()
+    kart_initial_position = [75, 75]
+    kart_initial_angle = 0
+
+    controller = AI()  # ou AI()
+    """
+    ==================== ATTENTION =====================
+    In the original requirement, the lines of code below
+    were indicated not to be changed.
+    ====================================================
+    """
+    kart = Kart(controller)
+    track = Track(track, kart_initial_position, kart_initial_angle)
+    track.add_kart(kart)
+    track.play()
+
+if __name__ == '__main__':
+    main()
